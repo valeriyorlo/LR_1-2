9f151ab7ed03f7d992cb171dd64d8503eed61acb
valeriyorlo <144387877+valeriyorlo@users.noreply.github.com>
2023-09-07 23:27:13 +0300

    Add files via upload

---
+++ блок 2 .2.pas
@@ -0,0 +1,9 @@
+﻿program ch;
+var n:integer;
+begin
+write('Введите число ');
+readln(n);
+n:=n mod 10;
+writeln('Последняяцифра в числе ', n);
+readln;
+end.
---
+++ блок 2.pas
@@ -0,0 +1,6 @@
+﻿program zad7;
+var n:integer;
+begin
+  n:=12345;
+  write(N mod 10);
+end.
